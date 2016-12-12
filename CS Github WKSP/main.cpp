2aa65c2781a34d26de5f32bce1b448a1a65b9017
Anutr Sivakoses <anutr.sivakoses@gmail.com>
2016-12-12 10:18:09 -0800

    added base project files

---
+++ CS Github WKSP/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Insert your name on the cout statement.
+    //Push changes
+    cout << "Hello world!" << endl;
+    return 0;
+}
